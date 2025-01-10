d6c38460a3fd26cb92d3fe8af11f518733ba05b3
chadrakdev <chadrak@outlook.com>
2025-01-10 01:59:41 +0000

    Add Game model/dto

---
+++ GameStoreApi/Models/Game.cs
@@ -0,0 +1,10 @@
+namespace GameStoreApi.Models;
+
+public class Game
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string Genre { get; set; }
+    public string Price { get; set; }
+}
